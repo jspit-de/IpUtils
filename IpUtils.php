3c35cd0ca9f7ff3ec0ec3d2a481ec60bdb29556b
jspit <pjunk@gmx.de>
2019-04-26 14:23:17 +0200

    Add files via upload

---
+++ IpUtils.php
@@ -0,0 +1,570 @@
+<?php
+/**
+.---------------------------------------------------------------------------.
+|  Software: class IpUtils : Utility Functions for IPv4/Ipv6                |
+|   Version: 1.0                                                            |
+|      Date: 2019-04-26                                                     |
+|       PHP: <= 5.6                                                         |
+| ------------------------------------------------------------------------- |
+| Copyright © 2019, Peter Junk (alias jspit). All Rights Reserved.          |
+' ------------------------------------------------------------------------- '
+*/
+
+class IpUtils
+{
+  //format constants
+  const BIN = "BIN";  //string binary "11110101.."
+  const DEC = "DEC";  //string decimal for bcmath
+  const HEX = "HEX";  //Hex-string with "0x" at first
+  const RAW = "RAW";  //string internal bin pton
+  const EXP = "EXP";  //expand output 0:0:567...
+  const COMP = "COMP"; //Compressed ::567:..
+  const FULL = "FULL"; //output 0000:0000:0567:...
+  const SUFFIX = "+";  //output with suffix
+  
+  private $binIp = null;
+  private $suffix = null;  //Suffix
+  private $ip;
+
+  /**
+   * @param mixed $ipInput 
+   */
+  public function __construct($ipInput)
+  {
+    if(is_string($ipInput)) {
+      //IPv4,IPv6,"decimal","0x_hex"
+      $ipsuffix = explode("/",$ipInput);
+      $this->ip = $ipsuffix[0];
+      if(isset($ipsuffix[1])) $this->suffix = (int)$ipsuffix[1];
+      $this->binIp = self::inetPtonFromString($this->ip);
+    }
+    elseif($ipInput instanceOf self){
+      //IpUtils Object
+      $stdObj = $ipInput->getDataObj();
+      $this->ip = $stdObj->ip;
+      $this->suffix = $stdObj->suffix;
+      $this->binIp = $stdObj->binIp;
+    }
+    elseif(is_int($ipInput)){
+      //integer only to IPv4
+      $this->ip = $ipInput;
+      $this->binIp = inet_pton(long2ip($ipInput)); 
+    }
+    else {
+      $msg = "Input could not use for IpUtils.";
+      throw new \InvalidArgumentException($msg);
+    }
+  }
+
+  /**
+   * Create Object
+   * @param string       $ipInput
+   * @return object IpUtils;
+   */
+  public static function create($ipInput)
+  {
+    return new static($ipInput);
+  }
+
+  /**
+   * setSuffix
+   * @param int $suffix
+   * @return $this or false if error
+   */
+  public function setSuffix($suffix)
+  {
+    $maxsuffix = strlen($this->binIp) == 16 ? 128 : 32;
+    if($suffix < 1 OR $suffix > $maxsuffix) return false;
+    $this->suffix = $suffix;
+    return $this;
+  }
+
+  /**
+   * return true if valid Ip V4
+   * @return bool;
+   */
+  public function isIp4()
+  {
+    return strlen($this->binIp) == 4;
+  }
+
+  /**
+   * return true if valid Ip V6
+   * @return bool;
+   */
+  public function isIp6()
+  {
+    return strlen($this->binIp) == 16;
+  }
+
+  /**
+   * return true if valid Ip (V4 or V6)
+   * @return bool;
+   */
+  public function isIp()
+  {
+    return $this->isIp4() OR $this->isIp6();
+  }
+  
+  /**
+   * Format Object
+   * @param string $format
+   * @return string
+   */
+  public function format($format = null)
+  {
+    if($ip = self::formatBinIp($this->binIp, $format)) {
+      if($format === null OR strpos($format,"+") !== false) {
+        $ip .= $this->suffix !== null ? ("/".$this->suffix) : "";
+      }  
+      return $ip;
+    }
+    return false;
+  }
+      
+  /**
+   * get suffix 
+   * @return int suffix or false if suffix not exists 
+   */
+  public function suffix()
+  {
+    return is_int($this->suffix)
+      ? $this->suffix
+      : false;
+  }
+  
+  /**
+   * get Hosts number 
+   * @return float number of hosts or false if ip invalid
+   */
+  public function hosts()
+  {
+    $bits = strlen($this->binIp) * 8;
+    if($bits == 0) return false;
+    if($this->suffix === null) return 1.0; 
+    $p = $bits - $this->suffix;
+    if($p < 0 OR $p > $bits) return false;
+    return (float)(2 ** $p);
+  }
+  
+
+  /**
+   * get Net Mask 
+   * @param string $format
+   * @return string netMask
+   */
+  public function netMask($format = null)
+  {
+    $binMask = self::suffixMask($this->suffix, strlen($this->binIp) * 8);
+    if($binMask === false) return false;
+    return self::formatBinIp($binMask, $format);
+  }
+
+  /**
+   * get Net Address 
+   * @param string $format
+   * @return string netMask
+   */
+  public function netAdr($format = null)
+  {
+    $range = $this->range($format);
+    return is_array($range) ? $range[0] : false; 
+  }
+
+  /**
+   * get Broadcast Address 
+   * @param string $format
+   * @return string netMask
+   */
+  public function broadcast($format = null)
+  {
+    $range = $this->range($format);
+    return is_array($range) ? $range[1] : false; 
+  }
+  
+  
+  /**
+   * get range as array with intern bin-ptop-strings
+   * @return mixed array [binIpFrom, binIpTo] or
+   *  false if intern binIp == false
+   */
+  public function binRange()
+  {
+    if(($binIp = $this->binIp) === false) {
+      return false; //invalid ip basis
+    }
+    elseif($this->suffix === null) {
+      return [$this->binIp, $this->binIp];
+    }
+    else {
+      $binMask = self::suffixMask($this->suffix, strlen($this->binIp) * 8);
+      if($binMask === false) return false;
+      return [
+        $this->binIp & $binMask,
+        $this->binIp | ~$binMask
+      ];
+    }  
+  }
+
+  /**
+   * get the range as array 
+   * @param string  $format (null,'dec','raw','full')
+   * @return array [IpFrom, IpTo], false if error
+   */
+  public function range($format = null)
+  {
+    if($binRange = $this->binRange()) {
+      return [
+        self::formatBinIp($binRange[0], $format),
+        self::formatBinIp($binRange[1], $format)
+      ];
+    }
+    return false;
+  }
+
+  /**
+   * set to minimal subnet with $ip
+   * @param mixed $ip
+   * @return $this
+   */
+  public function setMinSubnet($ip)
+  {
+    $ipData = self::create($ip)->getDataObj();
+    $binStrIp2 = $ipData->binIp;
+    $ip2Bytes = strlen($binStrIp2);
+    if($ip2Bytes == 0 OR $ip2Bytes != strlen($this->binIp)) {
+      //Error
+      $msg = "Invalid Ip";
+      throw new \InvalidArgumentException($msg);
+    }
+    $binStrX = self::ptonToBin($this->binIp ^ $binStrIp2);
+    $bits = strlen($binStrX);
+    $minsuffix = $bits - strlen(ltrim($binStrX ,"0")); 
+    $binMask = self::suffixMask($minsuffix, $bits);    
+    $this->binIp &= $binMask;
+    $this->suffix = $minsuffix;
+    return $this;
+  }
+
+  
+  /**
+   * get intercect range as array 
+   * empty array if not intersect or false if Error
+   * @param mixed $ip
+   * @param string  $format (null,'dec','raw','full')
+   * @return mixed array [IpFrom, IpTo], bool
+   */
+  public function rangeIntersect($ip, $format = null)
+  {
+    if($binRange1 = $this->binRange()) {
+      if($ip2 = self::create($ip)) {
+        if($binRange2 = $ip2->binRange()) {
+          $isec0 = max($binRange1[0], $binRange2[0]);
+          $isec1 = min($binRange1[1], $binRange2[1]);
+          if($isec0 > $isec1) return [];
+          return [
+            self::formatBinIp($isec0, $format),
+            self::formatBinIp($isec1, $format)
+          ];
+        }
+      }
+    }
+    return false;
+  }
+  
+ /*
+  * Add a integer offset to Ip
+  * @param int add
+  * @return object $this
+  */
+  public function addOffset($add)
+  {
+    $this->binIp = self::binStrAddInt($this->binIp, (int)$add);
+    return $this;
+  }  
+  
+  /**
+   * Returns the textual representation of the IP address.
+   * Empty String if Ip invalid
+   * @return string
+   */
+  public function __toString()
+  {
+    return (string)$this->format();
+  }
+
+  /**
+   * get Data as StdClass-Object
+   * @return object of StdClass
+   */
+  public function getDataObj(){
+    $obj = new stdClass;
+    $obj->ip = $this->ip;
+    $obj->suffix = $this->suffix;
+    $obj->binIp = $this->binIp;
+    return $obj;
+  }
+
+  
+  /**
+   * get Inet Pton Bin String
+   * @param string       $ip
+   * @return string inet_pton;
+   */
+  public static function inetPtonFromString($ip)
+  {
+    if(is_string($ip)){
+      if(filter_var($ip, FILTER_VALIDATE_IP)) {
+        //IPv4 AND IPv6
+        return inet_pton($ip);
+      }
+      elseif(preg_match('~^0x[0-9A-F]{1,32}$~i', $ip)){
+        //hex-format "0x00ffcd78"
+        return self::hexToPton($ip);
+      }
+      elseif(ctype_digit($ip)) { 
+        //dec bc string 
+        return self::decToPton($ip); 
+      }
+    }
+    return false;
+  }
+  
+ /*
+  * Remove leading zero in IP
+  * @param string IP
+  * @return string
+  */
+  public static function cleanIP($ip) {
+    return preg_replace('~(^|[.:])0{1,2}(\d)~','$1$2',$ip);
+  }
+  
+
+ /*
+  * @param $suffixIP ipv6/suffix or ipv4/suffix
+  * return array("Start-IP","End-IP") or false if error
+  */
+  public static function ipsuffixRange($suffixIP)
+  {
+    list($ip, $suffix) = explode('/', $suffixIP);
+    $ipBin = inet_pton($ip);
+    if($ipBin === false) {
+      return false;
+    }
+    //subnet-mask
+    $strMask = self::strMaskFromsuffix($suffix, strlen($ipBin)*8);
+    if($strMask === false) return false;
+ 
+    return [
+      inet_ntop($ipBin & $strMask),
+      inet_ntop($ipBin | ~$strMask)
+    ];
+  }
+  
+ 
+  /**
+   * Format internal binIp
+   * @param string  $binIp
+   * @param string  $format (null,'dec','raw','full')
+   * @return string if ok, false if error
+   */
+  public static function formatBinIp($binIp, $format = null)
+  {
+    if(!is_string($binIp)) return false;
+    
+    if($format === null OR stripos($format, SELF::COMP) !== false) {
+      return inet_ntop($binIp);
+    }
+
+    elseif(stripos($format, SELF::BIN) !== false) {
+      //binary string 
+      return self::ptonToBin($binIp);
+    }
+    
+    elseif(stripos($format, SELF::DEC) !== false) {
+      //decimal
+      return self::ptonToDec($binIp);
+    }
+
+    elseif(stripos($format, SELF::HEX) !== false) {
+      //hexadecimal
+      $strHex = ltrim(bin2hex($binIp),"0");
+      if(strlen($binIp) == 16) {
+        //add leading zeros for IPv6        
+        $strHex = str_pad($strHex,9,"0",STR_PAD_LEFT);
+      }
+      return "0x".$strHex;
+    }
+
+    elseif(stripos($format, SELF::RAW) !== false) {
+      //bin stream 
+      return $binIp;
+    }
+
+    elseif(stripos($format, SELF::EXP) !== false) {
+      if(strlen($binIp) == 4){ //v4
+        //without leading 0, because with leading 0 is invalid
+        return inet_ntop($binIp);      
+      }
+      elseif(strlen($binIp) == 16){
+        //v6 as xxxx:xxxx: .. without leading 0
+        return implode(":", array_map(
+          function($word){return sprintf("%x", $word);},
+          unpack('n*', $binIp)) //words
+        );
+      }
+    } 
+
+    elseif(stripos($format, SELF::FULL) !== false) {
+      if(strlen($binIp) == 4){ //v4
+        //without leading 0, because with leading 0 is invalid
+        return inet_ntop($binIp);      
+      }
+      elseif(strlen($binIp) == 16){
+        //v6 as xxxx:xxxx: ..
+        return implode(":", array_map(
+          function($word){return sprintf("%04x", $word);},
+          unpack('n*', $binIp)) //words
+        );
+      }
+    }        
+    
+    return false;
+  }
+      
+ /**
+  * create a binary string mask from suffix
+  * @param int $suffix range 1..bitBasis
+  * @param int $bitBasis, must be a multiple of 8, default 128
+  * @return string as packed inet_pton in_addr representation
+  */
+  public static function suffixMask($suffix, $bitBasis = 128)
+  {
+    if($suffix < 0 OR $suffix > $bitBasis OR $suffix === null ) {
+      return false;
+    }
+    $zeroBits = $bitBasis - $suffix;
+    $restBits = $zeroBits % 8;
+    $strMask = $restBits ? chr(0xff << $restBits) : "";
+    $strMask .= str_repeat("\x00",(int)($zeroBits/8));
+    return str_pad($strMask,(int)($bitBasis/8),"\xff",STR_PAD_LEFT);
+  }
+  
+  //inet_pton String to BCMath decimal string
+  public static function ptonToDec($ptonBinStr)
+  {
+    $arrInt16 = unpack("n*", $ptonBinStr);
+    
+    foreach($arrInt16 as $i => $value){
+      $bc = ($i == 1) ? $value : bcadd(bcmul($bc,"65536"),$value);
+    }
+
+    return $bc;
+  }
+
+  //BCMath decimal string to inet_pton String 
+  public static function decToPton($dec)
+  {
+    for($bin = "", $i=0; $dec != "0" AND $i < 8; ++$i){
+      $bin = pack("n",(int)bcmod($dec,"65536")).$bin;
+      $dec = bcdiv( $dec, "65536", 0 );    
+    }
+    return str_pad($bin,16,"\x00",STR_PAD_LEFT);
+  }
+  
+  //inet_pton String to binary string
+  public static function ptonToBin($ptonBinStr)
+  {
+    $arrInt16 = unpack("n*", $ptonBinStr);
+    
+    $binstr = "";
+    foreach($arrInt16 as $i => $value){
+      $binstr .= sprintf("%016b",$value);  
+    }
+    return $binstr;
+  }
+  
+
+ /** 
+  * hex string to inet_pton String 
+  * @param string how "0cde67ff"
+  * @return string pton byte straem 4 or 16 Byte
+  *  if input-len <= 8 result is for IPv4, else IPv6
+  *  return false if error  
+  */
+  public static function hexToPton($strHex)
+  {
+    if(stripos($strHex,"0x") === 0) $strHex = substr($strHex,2);
+    $lenForPton = strlen($strHex) <= 8 ? 8 : 32;
+    $strHex = str_pad($strHex,$lenForPton,"0",STR_PAD_LEFT);
+    return pack('H*',$strHex);
+  }
+
+  
+ /** 
+  * Add int offset to ip-pton-string
+  * @param string binStr    ("\x00\x00\x01\xff")
+  * @param int add          ( 1 )
+  * @return string          ("\x00\x00\x02\x00")
+  *  return false if error  
+  */
+  public static function binStrAddInt($binStr, $add){
+    $bytesBasis = strlen($binStr); //4 or 16
+    $binStrAdd = pack((PHP_INT_SIZE == 4 ? "N" : "J"),abs((int)$add)); //4 or 8 Byte
+    $binStrAdd = ltrim($binStrAdd,"\x00");
+    $binStrSum = $add >= 0 
+      ? self::binStrAdd($binStr, $binStrAdd)
+      : self::binStrSub($binStr, $binStrAdd)
+    ;
+    $bytesStrSum = strlen($binStrSum);
+    return $bytesStrSum == $bytesBasis ? $binStrSum : false;
+  }
+  
+ /** 
+  * Add ip-pton-strings
+  * @param string binStr    ("\x00\x00\x01\xff")
+  * @param string binStrAdd ("\x00\x00\x00\x01")
+  * @return string          ("\x00\x00\x02\x00")
+  *  return false if error  
+  */
+  public static function binStrAdd($binStr, $binStrAdd){
+    $len = max(strlen($binStr),strlen($binStrAdd));
+    $binStr = str_pad($binStr,$len,"\x00",STR_PAD_LEFT);
+    $binStrAdd = str_pad($binStrAdd,$len,"\x00",STR_PAD_LEFT);
+    
+    $overflow = 0;
+    for($i=$len-1; $i >= 0; $i--){
+      $iByte = ord($binStr[$i]) + ord($binStrAdd[$i]) + $overflow;
+      $binStr[$i] = chr($iByte % 256);
+      $overflow = (int)($iByte/256);
+    }
+    return $overflow ? false : $binStr;
+  }
+
+ /** 
+  * Sub ip-pton-strings
+  * @param string binStr    ("\x00\x00\x01\xff")
+  * @param string binStrSub ("\x00\x00\x00\x01")
+  * @return string          ("\x00\x00\x02\x00")
+  *  return false if error  
+  */
+  public static function binStrSub($binStr, $binStrSub){
+    $len = max(strlen($binStr),strlen($binStrSub));
+    $binStr = str_pad($binStr,$len,"\x00",STR_PAD_LEFT);
+    $binStrSub = str_pad($binStrSub,$len,"\x00",STR_PAD_LEFT);
+    
+    $overflow = 0;
+    for($i=$len-1; $i >= 0; $i--){
+      $iByte = ord($binStr[$i]) - ord($binStrSub[$i]) - $overflow;
+      $overflow = 0;
+      if($iByte < 0) {
+        $iByte += 256;
+        $overflow = 1;
+      }
+      $binStr[$i] = chr($iByte);
+    }
+    return $overflow ? false : $binStr;
+  }
+  
+
+}
---
+++ README.md
@@ -0,0 +1,111 @@
+# IpUtils
+
+IPv4 and IPv6 utility class
+
+## Usage
+
+### Input and Output
+
+```php
+require '/yourPath/IpUtils.php';  //or Autoload
+
+$ip = IpUtils::create('192.168.178.10/24');
+echo $ip->netMask();  //255.255.255.0
+```
+
+Supported Input Formats:
+
+- IPv4 string 192.168.178.56
+- IPv4 string with suffix 192.168.178.56/24
+- IPv6 string ::8f56:1
+- IPv6 string with suffix ::8f56:1/124
+- integer (only for IPv4, for Suffix use setSuffix())
+- Dec-String "123" (only for IPv6)
+- Hex-String "0xf" (IPv4, IPv6 if more than 10 chars ""0x00000000f")
+
+For Outputs use format()
+```php
+echo IpUtils::create('::1')->format('exp');  //0:0:0:0:0:0:0:1
+echo IpUtils::create('178.114.56.1/24')->format('hex');  //0xb2723801
+echo IpUtils::create('::8f56:1/120')->format('exp+'); //0:0:0:0:0:0:8f56:1/120
+```
+Supported Output Formats:
+
+- "BIN" : string binary "11110101.."
+- "DEC" : decimal string (may use for for bcmath)
+- "HEX" : Hex-String with "0x" at first
+- "RAW" : internal Bin-Pton-String (do not use with echo)
+- "EXP" : expand output 0:0:567...
+- "COMP": Compressed ::567:..
+- "FULL"; Output 0000:0000:0567:...
+
+With "+" the Suffix will add
+
+## Class-Info
+
+| Info | Value |
+| :--- | :---- |
+| Declaration | class IpUtils |
+| Datei | IpUtils.php |
+| Date/Time modify File | 2019-04-26 13:25:48 |
+| File-Size | 15 KByte |
+| MD5 File | a23ad83f3a7a431101fe6cde13717a30 |
+| Version | 1.0 |
+| Date | 2019-04-26 |
+
+## Public Methods
+
+| Methods and Parameter | Description/Comments |
+| :-------------------- | :------------------- |
+| public function __construct($ipInput) | @param mixed $ipInput  |
+| public static function create($ipInput) | Create Object<br>@param string $ipInput<br>@return object IpUtils; |
+| public function setSuffix($suffix) | setSuffix<br>@param int $suffix<br>@return $this or false if error |
+| public function isIp4() | return true if valid Ip V4<br>@return bool; |
+| public function isIp6() | return true if valid Ip V6<br>@return bool; |
+| public function isIp() | return true if valid Ip (V4 or V6)<br>@return bool; |
+| public function format($format = null) | Format Object<br>@param string $format<br>@return string |
+| public function suffix() | get suffix <br>@return int suffix or false if suffix not exists  |
+| public function hosts() | get Hosts number <br>@return float number of hosts or false if ip invalid |
+| public function netMask($format = null) | get Net Mask <br>@param string $format<br>@return string netMask |
+| public function netAdr($format = null) | get Net Address <br>@param string $format<br>@return string netMask |
+| public function broadcast($format = null) | get Broadcast Address <br>@param string $format<br>@return string netMask |
+| public function binRange() | get range as array with intern bin-ptop-strings<br>@return mixed array [binIpFrom, binIpTo] or<br>false if intern binIp == false |
+| public function range($format = null) | get the range as array <br>@param string $format (null,&#039;dec&#039;,&#039;raw&#039;,&#039;full&#039;)<br>@return array [IpFrom, IpTo], false if error |
+| public function setMinSubnet($ip) | set to minimal subnet with $ip<br>@param mixed $ip<br>@return $this |
+| public function rangeIntersect($ip, $format = null) | get intercect range as array <br>empty array if not intersect or false if Error<br>@param mixed $ip<br>@param string $format (null,&#039;dec&#039;,&#039;raw&#039;,&#039;full&#039;)<br>@return mixed array [IpFrom, IpTo], bool |
+| public function addOffset($add) | Add a integer offset to Ip<br>@param int add<br>@return object $this |
+| public function __toString() | Returns the textual representation of the IP address.<br>Empty String if Ip invalid<br>@return string |
+| public function getDataObj() | get Data as StdClass-Object<br>@return object of StdClass |
+| public static function inetPtonFromString($ip) | get Inet Pton Bin String<br>@param string $ip<br>@return string inet_pton; |
+| public static function cleanIP($ip) | Remove leading zero in IP<br>@param string IP<br>@return string |
+| public static function ipsuffixRange($suffixIP) | @param $suffixIP ipv6/suffix or ipv4/suffix<br>return array(&quot;Start-IP&quot;,&quot;End-IP&quot;) or false if error |
+| public static function formatBinIp($binIp, $format = null) | Format internal binIp<br>@param string $binIp<br>@param string $format (null,&#039;dec&#039;,&#039;raw&#039;,&#039;full&#039;)<br>@return string if ok, false if error |
+| public static function suffixMask($suffix, $bitBasis = 128) | create a binary string mask from suffix<br>@param int $suffix range 1..bitBasis<br>@param int $bitBasis, must be a multiple of 8, default 128<br>@return string as packed inet_pton in_addr representation |
+| public static function ptonToDec($ptonBinStr) | inet_pton String to BCMath decimal string |
+| public static function decToPton($dec) | BCMath decimal string to inet_pton String  |
+| public static function ptonToBin($ptonBinStr) | inet_pton String to binary string |
+| public static function hexToPton($strHex) | hex string to inet_pton String <br>@param string how &quot;0cde67ff&quot;<br>@return string pton byte straem 4 or 16 Byte<br>if input-len &lt;= 8 result is for IPv4, else IPv6<br>return false if error  |
+| public static function binStrAddInt($binStr, $add) | Add int offset to ip-pton-string<br>@param string binStr (&quot;\x00\x00\x01\xff&quot;)<br>@param int add ( 1 )<br>@return string (&quot;\x00\x00\x02\x00&quot;)<br>return false if error  |
+| public static function binStrAdd($binStr, $binStrAdd) | Add ip-pton-strings<br>@param string binStr (&quot;\x00\x00\x01\xff&quot;)<br>@param string binStrAdd (&quot;\x00\x00\x00\x01&quot;)<br>@return string (&quot;\x00\x00\x02\x00&quot;)<br>return false if error  |
+| public static function binStrSub($binStr, $binStrSub) | Sub ip-pton-strings<br>@param string binStr (&quot;\x00\x00\x01\xff&quot;)<br>@param string binStrSub (&quot;\x00\x00\x00\x01&quot;)<br>@return string (&quot;\x00\x00\x02\x00&quot;)<br>return false if error  |
+
+## Constants
+
+| Declaration/Name | Value | Description/Comments |
+| :--------------- | :---- | :------------------- |
+|  const BIN = &quot;BIN&quot;; //string binary &quot;11110101..&quot; | &#039;BIN&#039; |  format constants<br>string binary &quot;11110101..&quot;  |
+|  const DEC = &quot;DEC&quot;; //string decimal for bcmath | &#039;DEC&#039; |  string decimal for bcmath  |
+|  const HEX = &quot;HEX&quot;; //Hex-string with &quot;0x&quot; at first | &#039;HEX&#039; |  Hex-string with &quot;0x&quot; at first  |
+|  const RAW = &quot;RAW&quot;; //string internal bin pton | &#039;RAW&#039; |  string internal bin pton  |
+|  const EXP = &quot;EXP&quot;; //expand output 0:0:567... | &#039;EXP&#039; |  expand output 0:0:567...  |
+|  const COMP = &quot;COMP&quot;; //Compressed ::567:.. | &#039;COMP&#039; |  Compressed ::567:..  |
+|  const FULL = &quot;FULL&quot;; //output 0000:0000:0567:... | &#039;FULL&#039; |  output 0000:0000:0567:...  |
+|  const SUFFIX = &quot;+&quot;; //output with suffix | &#039;+&#039; |  output with suffix  |
+
+### Requirements
+
+- PHP 5.6+
+
+## Thanks
+
+This work was heavily inspired by [Dormilich/network-objects](https://github.com/Dormilich/network-objects).
